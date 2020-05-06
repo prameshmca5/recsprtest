9f3dc437516be3000aaef3bee7074891fe51841b
Ramesh <rameshkp2@gmail.com>
2020-05-06 11:18:02 +0800

    Created file into server.

--- src/main/java/com/recspr/spring/recspring/RecspringApplication.java
+++ src/main/java/com/recspr/spring/recspring/RecspringApplication.java
@@ -1,13 +1,19 @@
 package com.recspr.spring.recspring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class RecspringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecspringApplication.class, args);
+
+		System.out.println("Welcome java boot");
 	}
+
 
 }
